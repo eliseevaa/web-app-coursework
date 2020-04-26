ea0156d71f6b0cbd9bd9fbc5aaa9138a6b8db604
Anastasiya <anastasiyaeliseeva@anastasiyasmbp3.mynet>
2020-04-26 12:28:48 +0100

    section selected by default + swapped last 2 sections of the page

--- inm316/project/project.js
+++ inm316/project/project.js
@@ -1,29 +1,34 @@
 // initial setup: add a listener to buttons
 var cardButtonElementsGlobal = document.querySelectorAll(".card-button");
 cardButtonElementsGlobal.forEach(addCardButtonListener);
+updateUISelectingButton(cardButtonElementsGlobal[0]);
 
 // adding listener
 function addCardButtonListener(cardButtonElement) {
-    cardButtonElement.addEventListener("click", cardButtonClick);
+    cardButtonElement.addEventListener("click", handleCardButtonClick);
 }
 
 // event handling
-function cardButtonClick(event) {
+function handleCardButtonClick(event) {
     var clickedButton = event.currentTarget;
+    updateUISelectingButton(clickedButton)
+}
 
+// UI update depending on selected button passed as an argument
+function updateUISelectingButton(button) {
     // first update the buttons
 
     // remove selected state from all buttons
     cardButtonElementsGlobal.forEach(clearClickedButtonState);
     // add selected state just to the clicked button
-    clickedButton.classList.add("currently-selected-button");
+    button.classList.add("currently-selected-button");
 
 
     // next update the details divs
 
     // generate the class of the selected card's details element
     // for example: ".card-details-Venus" or ".card-details-Earth"
-    var detailsElementIdSelector = clickedButton.id;
+    var detailsElementIdSelector = button.id;
     var detailsElementId = "div" + detailsElementIdSelector;
 
     // find this element details
@@ -42,4 +47,6 @@ function clearClickedButtonState(cardButtonElement) {
 function clearCardDetailState(cardDetailElement) { console.log (cardDetailElement)
     cardDetailElement.style.display= "none"
 }
+
+
 
