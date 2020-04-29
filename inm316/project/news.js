3c6662676d3b1e13e3b9d169523eb0187e222666
Anastasiya <anastasiyaeliseeva@anastasiyasmbp3.mynet>
2020-04-29 12:18:24 +0100

    news content fixed

--- inm316/project/news.js
+++ inm316/project/news.js
@@ -27,7 +27,57 @@ function populateArticleSection(articles) {
         articleElement.id = makeUniqueIdForArticle(article);
         articleElement.classList.add("article");
         newsSection.appendChild(articleElement);
+
+        console.log(article);
+
+        var articleDetailsElement = document.createElement ("div");
+        articleDetailsElement.classList.add("article-details");
+        articleElement.appendChild(articleDetailsElement)
+
+        var articleImage = document.createElement("img");
+        articleImage.classList.add("article-image");
+        articleImage.src = article.urlToImage;
+        articleDetailsElement.appendChild(articleImage);
+
+        var articleBodyDetailsElement = document.createElement ("div");
+        articleBodyDetailsElement.classList.add("article-body");
+        articleDetailsElement.appendChild(articleBodyDetailsElement)
+
+        var articleBodyTitleDetailsElement = document.createElement ("div");
+        articleBodyTitleDetailsElement.classList.add("title-author");
+        articleBodyDetailsElement.appendChild(articleBodyTitleDetailsElement)
+        
+        var articleTitle= document.createElement("h4")
+        articleTitle.classList.add("article-title");
+        articleTitle.textContent = article.title;
+        articleBodyTitleDetailsElement.appendChild(articleTitle);
+
+        var articleAuthor= document.createElement("p")
+        articleAuthor.classList.add("label-text");
+        articleAuthor.textContent = article.author;
+        articleBodyTitleDetailsElement.appendChild(articleAuthor);
+
+        var articleDescription= document.createElement("p")
+        articleDescription.classList.add("article-description");
+        articleDescription.textContent = article.description;
+        articleBodyTitleDetailsElement.appendChild(articleDescription);
+
+        var articleDate = document.createElement("p");
+        articleDate.classList.add("date");
+        // var formattedDate = new Date(comment.date).toLocaleDateString("en-UK")
+        articleDate.textContent = "Date: " + article.publishedAt;
+        articleBodyDetailsElement.appendChild(articleDate);
+
+        
+
     });
 }
+
+tool(article);
+
+function tool(article) {
+    link.title = articleTitle.textContent;
+  }
+
 
 
