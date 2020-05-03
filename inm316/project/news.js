bbfacf20cd3508ceccd2df9d8852e24bfdec133c
Anastasiya <anastasiyaeliseeva@anastasiyasmbp3.mynet>
2020-05-03 18:07:43 +0100

    filter is fixed

--- inm316/project/news.js
+++ inm316/project/news.js
@@ -1,20 +1,28 @@
 var url = 'http://newsapi.org/v2/everything?' +
     'q=online courses;public-speaking;&' +
-    'from=2020-04-01&' +
+    'from=2020-0502&' +
     'sortBy=popularity&' +
     'apiKey=78c620b4a14942c2b4983728d24cc0e9';
 
 var req = new Request(url);
+var articlesData;
 
 fetch(req)
     .then((response) => response.json())
     .then((data) => {
         console.log(data);
-        populateArticleSection(data.articles);
+        var articles = data.articles;
+        for (var i in articles) {
+            if (!articles[i].id) {
+                articles[i].id = Math.random().toString(36).substring(2, 15);
+            }
+        }
+        articlesData = articles;
+        populateArticleSection(articlesData);
     });
 
-function makeUniqueIdForArticle(comment) {
-    return "article_" + comment.id;
+function makeUniqueIdForArticle(article) {
+    return "article_" + article.id;
 }
 
 function populateArticleSection(articles) {
@@ -23,46 +31,46 @@ function populateArticleSection(articles) {
 
     //creating a div for each article in the carousel  
     articles.forEach(function (article) {
-        var articleElement = document.createElement("div");
-        articleElement.id = makeUniqueIdForArticle(article);
-        articleElement.classList.add("article");
-        newsSection.appendChild(articleElement);
+        var articleDiv = document.createElement("div");
+        articleDiv.id = makeUniqueIdForArticle(article);
+        articleDiv.classList.add("article");
+        newsSection.appendChild(articleDiv);
 
         console.log(article);
 
-        var articleDetailsElement = document.createElement ("div");
+        var articleDetailsElement = document.createElement("div");
         articleDetailsElement.classList.add("article-details");
-        articleElement.appendChild(articleDetailsElement)
+        articleDiv.appendChild(articleDetailsElement)
 
         var articleImage = document.createElement("img");
         articleImage.classList.add("article-image");
         articleImage.src = article.urlToImage;
         articleDetailsElement.appendChild(articleImage);
 
-        var articleBodyDetailsElement = document.createElement ("div");
+        var articleBodyDetailsElement = document.createElement("div");
         articleBodyDetailsElement.classList.add("article-body");
         articleDetailsElement.appendChild(articleBodyDetailsElement)
 
-        var articleBodyTitleDetailsElement = document.createElement ("div");
+        var articleBodyTitleDetailsElement = document.createElement("div");
         articleBodyTitleDetailsElement.classList.add("title-author");
         articleBodyDetailsElement.appendChild(articleBodyTitleDetailsElement)
-        
-        var a = document.createElement('a');  
+
+        var a = document.createElement('a');
         a.title = article.title;
         a.href = article.url;
         articleBodyTitleDetailsElement.appendChild(a);
 
-        var articleTitle= document.createElement("h4")
+        var articleTitle = document.createElement("h4")
         articleTitle.classList.add("article-title");
         articleTitle.textContent = article.title;
         a.appendChild(articleTitle);
 
-        var articleAuthor= document.createElement("p")
+        var articleAuthor = document.createElement("p")
         articleAuthor.classList.add("article-author");
         articleAuthor.textContent = article.author;
         articleBodyTitleDetailsElement.appendChild(articleAuthor);
 
-        var articleDescription= document.createElement("p")
+        var articleDescription = document.createElement("p")
         articleDescription.classList.add("article-description");
         articleDescription.textContent = article.description;
         articleBodyTitleDetailsElement.appendChild(articleDescription);
@@ -76,12 +84,89 @@ function populateArticleSection(articles) {
         articleDate.textContent = "Published: " + `${da} ${mo} ${ye}`;
         articleBodyDetailsElement.appendChild(articleDate);
 
+
     });
 }
 
+// setup search
 
+// listen to changes in the search form
+var searchInput = document.querySelector(".search");
+searchInput.addEventListener("input", updateSearchValue);
 
+// initial search value, which will be empty
+var searchValue = "";
 
+// check what search term has been entered
+function updateSearchValue() {
+    // trim() removes any spaces before/after the input
+    searchValue = searchInput.value.trim();
 
+    // loop through all elements
+    // show or hide each one based on the search term
+    var articleDivs = document.querySelectorAll(".article");
+    articleDivs.forEach(showOrHideArticle);
+}
 
+// every time the text field changes, run this
+function showOrHideArticle(articleDiv) {
+    // if no search value is set, show the articles
+    if (searchValue.length === 0) {
+        articleDiv.classList.remove("hide");
+    } else {
+        // get the title of the article from its article element
+        // first find the article
 
+        for (var key in articlesData) {
+            var article = articlesData[key];
+
+            if (makeUniqueIdForArticle(article) === articleDiv.id) {
+                // found matching article, now check its data
+                if (checkArticleMatchesSearchQuery(article, searchValue)) {
+                    articleDiv.classList.remove("hide");
+                } else {
+                    articleDiv.classList.add("hide");
+                }
+                break;
+            }
+        }
+    }
+}
+
+function checkArticleMatchesSearchQuery(article, searchValue) {
+    // search is based on 3 criteria, and we are looking for an OR match
+
+    // first check title match
+    if (checkDataPropertyContainsSearchValue(article.title, searchValue)) {
+        return true;
+    }
+
+    // next, check author name match
+    if (checkDataPropertyContainsSearchValue(article.author, searchValue)) {
+        return true;
+    }
+
+    // last, check description match
+    if (checkDataPropertyContainsSearchValue(article.description, searchValue)) {
+        return true;
+    }
+
+    // no match, return false
+    return false;
+}
+
+function checkDataPropertyContainsSearchValue(dataProperty, searchValue) {
+    return dataProperty && dataProperty.toLowerCase().includes(searchValue.toLowerCase());
+
+    if (dataProperty) {
+        if (dataProperty.toLowerCase().includes(searchValue.toLowerCase())) {
+            return true;
+            
+        } else {
+            return false
+        };
+
+    } else {
+        return false
+    };
+}
