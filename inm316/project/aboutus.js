a29daee9a86bf440f13b4cf3d7b990ac6274bc6b
Anastasiya <anastasiyaeliseeva@anastasiyasmbp3.mynet>
2020-05-09 15:42:22 +0100

    commit-fixed responsive for first blocks iphone 7

--- inm316/project/aboutus.js
+++ inm316/project/aboutus.js
@@ -7,7 +7,7 @@ function initGoogleMap() {
     var uluru = {lat: 51.4809440, lng:-0.1281286};
     
     var map = new google.maps.Map(
-        document.getElementById('map'), { zoom: 15, center: uluru });
+        document.getElementById('map2'), { zoom: 15, center: uluru });
         
     var marker = new google.maps.Marker({position: uluru, map: map});
 }
